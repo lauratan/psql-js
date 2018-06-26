09d5b1d21ad85c8ae44a1fe02bd3f1bf553c1d40
lauratan <laura.tan108@gmail.com>
2018-06-26 22:32:45 +0000

    fixed knex.insert

--- add_person.js
+++ add_person.js
@@ -13,12 +13,19 @@ const knex = require('knex')({
 const firstName = process.argv[2];
 const lastName = process.argv[3];
 const dob = process.argv[4];
-knex.select('first_name', 'last_name', 'birthdate').from('famous_people')
-.insert({first_name: firstName, last_name: lastName, birthdate: dob}).into('famous_people')
-.catch(function(error){
-    console.error(error)
-    return knex.destroy();
-});
+
+knex('famous_people')
+.insert({first_name: firstName, last_name: lastName, birthdate: dob})
+    .asCallback(function(err, rows) {
+        if (err) return console.error(err);
+        return knex.destroy()
+    });
+// knex.select('first_name', 'last_name', 'birthdate').from('famous_people')
+// .insert({first_name: firstName, last_name: lastName, birthdate: dob}).into('famous_people')
+// .catch(function(error){
+//     console.error(error)
+//     return knex.destroy();
+// });
 
 
 
